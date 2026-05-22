4602c8e710d2e22ba0f6b8d8b63df3e06ced67fc
Loise Musungu <156400478+loisemusungu@users.noreply.github.com>
2026-05-22 09:49:59 +0300

    added delete function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
- 
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
@@ -54,4 +54,13 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`
+    DELETE FROM invoices
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
 }
